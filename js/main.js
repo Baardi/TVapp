9337d71ea3807cdf53b10abf1c3cdbdf0c914bb5
Bård Sigurd Møller <git@bsmoller.no>
2026-08-17 02:21:50 +0200

    Set epg source for Frikanalen and Red Bull TV

--- js/main.js
+++ js/main.js
@@ -32,10 +32,10 @@ document.addEventListener('DOMContentLoaded', function() {
         { name: 'NRK Nett-TV 4', url: 'https://nrk-live-no.akamaized.net/nrktv7/muxed.m3u8' },        
         
         // Frikanalen
-        { name: 'Frikanalen', epgId: 'Frikanalen.no', url: 'https://frikanalen.no/stream/index.m3u8' },
+        { name: 'Frikanalen', epgId: 'Frikanalen.no', epgSource: 'norge', url: 'https://frikanalen.no/stream/index.m3u8' },
         
         // Red Bull TV
-        { name: 'Red Bull TV', epgId: '10001', url: 'https://rbmn-live.akamaized.net/hls/live/590964/BoRB-AT/master.m3u8' }
+        { name: 'Red Bull TV', epgId: '10001', epgSource: 'redbull', url: 'https://rbmn-live.akamaized.net/hls/live/590964/BoRB-AT/master.m3u8' }
                 
     ];
     var currentTvChannelIndex = 0;
